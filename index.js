9f91c9a39c514e257baba702b98f3f83ee505763
Nathaniel <nathaniel.jaden@student.moringaschool.com>
2024-03-15 10:50:24 +0300

    Looping lab

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solutions in this file
+let names = ["Guadalupe","Ollie","Aki"];
+let event = "birthday"
+
+function writeCards(names, event) {
+    const message = [];
+  for (let i = 0; i < names.length; i++) {
+   const message = `Thank you ${names[i]} for the wonderful ${event} gifts`
+     
+    messages.push(message);
+  }
+  return message ;
+}
+
+function countDown () {
+    let countDown = 10
+    while (countDown >= 0) {
+        console.log(countDown--)
+
+    }
+}
