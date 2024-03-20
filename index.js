d25c594b286e4a9dce19d81d14558559d718c8a2
Nathaniel <nathaniel.jaden@student.moringaschool.com>
2024-03-20 15:34:41 +0300

    Looping Lab

--- index.js
+++ index.js
@@ -1,20 +1,19 @@
-let names = ["Guadalupe","Ollie","Aki"];
-let event = "birthday"
+const names = ["Guadalupe", "Ollie", "Aki"];
+let event = "birthday";
 
 function writeCards(names, event) {
-    const message = [];
+  const message = [];
   for (let i = 0; i < names.length; i++) {
-   const message = `Thank you ${names[i]} for the wonderful ${event} gifts`
-     
-    messages.push(message);
+    message.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
   }
-  return message ;
+  return message;
 }
+const message = writeCards(names, event);
+console.log(message);
 
-function countDown () {
-    let countDown = 10
-    while (countDown >= 0) {
-        console.log(countDown--)
-
-    }
+function countDown() {
+  let countDown = 10;
+  while (countDown >= 0) {
+    console.log(countDown--);
+  }
 }
